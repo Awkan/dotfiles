c102b48d6d6b13b6ecd719629bdd2f71ad21b5dd
Donovan Bourlard <donovan@awkan.fr>
2020-02-05 20:21:03 +0100

    Install oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 # Install some basic packages
+echo -e "\n \e[32m Install some basic packages \e[97m \n"
 sudo apt install -y \
     git \
     tmux \
-    vim
+    vim \
+    wget
+
+# [oh-my-zsh] Install & default shell config
+echo -e "\n \e[32m oh-my-zsh : install & default shell config \e[97m \n"
+sudo apt install -y zsh
+sudo chsh -s "$(which zsh)" root
+echo -e "\n \e[32m Default SHELL is ${SHELL} \e[97m \n"
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 # Create symlinks to every files contained in dotfiles directory
+echo -e "\n \e[32m Create symlinks \e[97m \n"
 for f in $(ls -1 ~/dotfiles); do ln -fs ~/dotfiles/$f ~/.$f; done
