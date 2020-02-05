a39e85150346d6efbce183f45da0cd6a7df452e6
Donovan Bourlard <donovan@awkan.fr>
2020-02-05 19:29:09 +0100

    Add some basic packages installation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+# Install some basic packages
+sudo apt install -y \
+    git \
+    tmux \
+    vim
+
 # Create symlinks to every files contained in dotfiles directory
 for f in $(ls -1 ~/dotfiles); do ln -fs ~/dotfiles/$f ~/.$f; done
