d06c182511bb6640ec5a2fab248f65ee3b7b28d1
Donovan BOURLARD <donovan.bourlard@yousign.com>
2025-09-07 17:45:32 +0200

    Add docker compose

--- _init/docker.sh
+++ _init/docker.sh
@@ -16,7 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #   stable"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   disco \
+   noble \
    stable"
 
 # INSTALL DOCKER ENGINE - COMMUNITY
@@ -39,6 +39,19 @@ sudo usermod -aG docker $USER
 # CHECK INSTALLATION
 # ==================
 docker run hello-world
+
+# INSTALL PLUGINS
+# ===============
+
+# Compose
+
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.39.2/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+docker compose version
 
 echo -e "${green} ...Done${default}"
 
